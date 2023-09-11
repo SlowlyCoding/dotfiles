5455376c65324b0ab87c8b793a54a5cae241957b
SlowlyCoding <59534476+SlowlyCoding@users.noreply.github.com>
2023-09-11 21:32:54 +0200

    Update navbuddy.lua

--- home_dir/.config/nvim/lua/plugin_configs/navbuddy.lua
+++ home_dir/.config/nvim/lua/plugin_configs/navbuddy.lua
@@ -7,9 +7,7 @@ navbuddy.setup {
     },
     use_default_mappings = true,
     mappings = {
-        ["k"] = actions.next_sibling,     -- down
-        ["j"] = actions.previous_sibling, -- up
-        ["K"] = actions.move_down,        -- Move focused node down
-        ["J"] = actions.move_up,          -- Move focused node up
+        ["k"] = actions.next_sibling(),     -- down
+        ["j"] = actions.previous_sibling(), -- up
     },
 }
